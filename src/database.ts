7d8bf7c7552d091626a7869c7cef42b94cc0eed4
Kelvin Seibt <kelvin.seibt@gmail.com>
2023-07-12 10:21:50 -0300

    Find error on deploy

--- src/database.ts
+++ src/database.ts
@@ -5,19 +5,19 @@ if (!process.env.DATABASE_URL) {
   throw new Error('DATABASE_URL env not found.')
 }
 
-const connection =
-  env.DATABASE_CLIENT === 'sqlite'
-    ? { filename: env.DATABASE_URL }
-    : env.DATABASE_URL
-
 export const config: Knex.Config = {
   client: env.DATABASE_CLIENT,
-  connection,
+  connection:
+    env.DATABASE_CLIENT === 'sqlite'
+      ? { filename: env.DATABASE_URL }
+      : env.DATABASE_URL,
   useNullAsDefault: true,
   migrations: {
     extension: 'ts',
     directory: './db/migrations',
   },
 }
+
+console.log(config)
 
 export const knex = setupKnex(config)
